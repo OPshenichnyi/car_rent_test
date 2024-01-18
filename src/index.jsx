f2d7901b44e06900dcc7ccbf9f7bbf72598df557
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-01-18 12:27:48 +0200

    add seting

--- src/index.jsx
+++ src/index.jsx
@@ -6,10 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/app-return-pack">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// basename = "/app-return-pack"
+// basename="/car_rent_test"
